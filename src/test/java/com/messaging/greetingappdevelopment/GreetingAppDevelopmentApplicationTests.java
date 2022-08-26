35223cfd90d1dd379230e7b9a4fd7127f7faad6a
tejuypatil <tejuypatil@gmail.com>
2022-08-26 14:23:41 +0530

    Added Program

---
+++ src/test/java/com/messaging/greetingappdevelopment/GreetingAppDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messaging.greetingappdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
